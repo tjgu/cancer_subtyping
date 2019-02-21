e845dc64376f0307aac53ee688de9ea2cfc243f2
tgu <tgu@ufl.edu>
2019-02-21 11:42:18 -0500

    Add files via upload

--- cors_save_test.py
+++ cors_save_test.py
@@ -42,11 +42,11 @@ def test_cor(model_trained_2, model_trained_21, model_trained_1, model_trained_m
         file_handle=file(filename,'w')
         np.savetxt(file_handle, com2_h, delimiter="\t", header=" the values in the second combined hidden layer")
 	
-	[mirna_ave1_t, mirna_ave2_t] = cors_save2.mirna_cor_save(model_trained_2, model_trained_21, model_trained_1, model_trained_mirna, com2_h, mirna1_h, mirna_expr_org_test, result_dir)
-	[prt_ave1_t, prt_ave2_t] = cors_save2.prt_cor_save(model_trained_2, model_trained_21, model_trained_1, model_trained_prt, com2_h, prt1_h, prt_expr_org_test, result_dir)
-        [gn_ave1_t, gn_ave2_t] = cors_save2.gn_cor_save(model_trained_2, model_trained_21, model_trained_1, model_trained_1_gn, model_trained_2_gn, com2_h, gn2_h, gn_expr_org_test, result_dir)
-        [methy_ave1_t, methy_ave2_t] = cors_save2.methy_cor_save(model_trained_2, model_trained_21, model_trained_1, model_trained_1_methy, model_trained_2_methy, com2_h, methy2_h, methy_expr_org_test, result_dir)
-        [cna_ave1_t, cna_ave2_t] = cors_save2.cna_cor_save(model_trained_2, model_trained_21, model_trained_1, model_trained_1_cna, model_trained_2_cna, com2_h, cna2_h, cna_expr_org_test, result_dir)
+	[mirna_ave1_t, mirna_ave2_t] = cors_save.mirna_cor_save(model_trained_2, model_trained_21, model_trained_1, model_trained_mirna, com2_h, mirna1_h, mirna_expr_org_test, result_dir)
+	[prt_ave1_t, prt_ave2_t] = cors_save.prt_cor_save(model_trained_2, model_trained_21, model_trained_1, model_trained_prt, com2_h, prt1_h, prt_expr_org_test, result_dir)
+        [gn_ave1_t, gn_ave2_t] = cors_save.gn_cor_save(model_trained_2, model_trained_21, model_trained_1, model_trained_1_gn, model_trained_2_gn, com2_h, gn2_h, gn_expr_org_test, result_dir)
+        [methy_ave1_t, methy_ave2_t] = cors_save.methy_cor_save(model_trained_2, model_trained_21, model_trained_1, model_trained_1_methy, model_trained_2_methy, com2_h, methy2_h, methy_expr_org_test, result_dir)
+        [cna_ave1_t, cna_ave2_t] = cors_save.cna_cor_save(model_trained_2, model_trained_21, model_trained_1, model_trained_1_cna, model_trained_2_cna, com2_h, cna2_h, cna_expr_org_test, result_dir)
 
 	rcor2_t = 5 - mirna_ave2_t - prt_ave2_t - gn_ave2_t - methy_ave2_t - cna_ave2_t
 	rcor1_t = 5 - mirna_ave1_t - prt_ave1_t - gn_ave1_t - methy_ave1_t - cna_ave1_t
